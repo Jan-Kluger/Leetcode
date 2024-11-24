2ed816850e4c946be9d07556c1946798826b7e37
yannick <yannick@schuermann.org>
2024-11-24 09:50:56 +0100

    bla

--- num_recent_calls.ml
+++ num_recent_calls.ml
@@ -1,13 +1,21 @@
-module Solution = struct
-  
+module RecentCounter : sig
+  type t
 
-  let ping (channel : int Queue.t) (n : int) = failwith "TODO"
-  
+  val create : unit -> t
+  val ping : t -> int -> (int * t)
+end = struct
+  type t = int list
+
+  let create () = []
+
+  let ping requests t =
+    let rec filter_recent acc = function
+      | [] -> List.rev acc
+      | x :: xs ->
+        if x >= t - 3000 then filter_recent (x :: acc) xs
+        else filter_recent acc xs
+    in
+    let recent_requests = filter_recent [] requests in
+    let updated_requests = recent_requests @ [t] in
+    (List.length updated_requests, updated_requests)
 end
-
-let test_queue = Queue.create
-
-let new_q = Queue.add test_queue 1
-
-
-
